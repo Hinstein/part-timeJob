3798c80e3f4198d6dba795103597fd1807bcb511
Hinstein <1191915168@qq.com>
2019-03-05 21:50:12 +0800

    2019.3.5 添加拦截器

---
+++ src/main/java/com/parttimejob/component/LoginHandlerInterceptor.java
@@ -0,0 +1,34 @@
+package com.parttimejob.component;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.event.HierarchyBoundsListener;
+import java.awt.event.HierarchyEvent;
+
+/**
+ * @BelongsProject: part-timeJob
+ * @BelongsPackage: com.parttimejob.component
+ * @Author: Hinstein
+ * @CreateTime: 2019-03-05 12:38
+ * @Description:登录检查
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+
+    /**
+     * 拦截器
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object user = request.getSession().getAttribute("username");
+        if (user == null) {
+            //未登录，返回登录页面
+            request.getRequestDispatcher("/workerLogin").forward(request, response);
+            return false;
+        } else {
+            //已登录，放行请求
+            return true;
+        }
+    }
+}
---
+++ src/main/java/com/parttimejob/config/MyMvcConfig.java
@@ -0,0 +1,41 @@
+package com.parttimejob.config;
+
+import com.parttimejob.component.LoginHandlerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @BelongsProject: part-timeJob
+ * @BelongsPackage: com.parttimejob.config
+ * @Author: Hinstein
+ * @CreateTime: 2019-03-05 13:25
+ * @Description:
+ */
+
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/index");
+    }
+
+    /**
+     * 注册拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //静态资源也会被拦截，所以需要加"/assets/**","/webjars/**"
+        String[] excludeUrl = new String[]{"/index", "/assets/**", "/webjars/**", "/blog/**"
+                , "/workerLogin", "/managerLogin", "/workerRegister", "/managerRegister",
+                "/admin/login", "/manager/register", "/manager/login", "/worker/register",
+                "/worker/login", "/webjars/**"};
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns(excludeUrl);
+    }
+}
